e9a3681ed2a5838e3f777775724e93eb671a5c45
brompo <brompo@gmail.com>
2016-12-12 15:14:27 +0300

    implemented a header, and multiple components to display add header and categories

--- index.android.js
+++ index.android.js
@@ -1,11 +1,46 @@
 import React,{Component} from 'react';
-import {Text, View, StyleSheet, ListView, AppRegistry} from 'react-native';
+import {Text, View, StyleSheet, ListView, ScrollView, AppRegistry, TextInput} from 'react-native';
 
-export default class ListViewDemo extends Component{
+export default class PrayersView extends Component{
+  constructor(){
+    super();
+    this.state = {text:''};
+  }
+
+  render() {
+    return (
+      <View >
+      <Text style={styles.header}>Sala za Kibahai</Text>
+      <Text style={styles.detailheader}>Sala za Faradhi </Text>
+      <Text style={styles.detail}>Sala za Faradhi</Text>
+      <Text style={styles.detailheader}>{this.props.name}</Text>
+      </View>
+      );
+  }
+}
+class PrayerCategoryHeader extends Component{
+  render() {
+    return (
+      <Text style={styles.header}>Sala za Kibahai</Text>
+    );
+  }
+}
+
+class CategoryDetail extends Component{
+  render() {
+    return (
+      <Text style={styles.detail}>{this.props.name}</Text>
+    );
+  }
+}
+
+class prayers extends Component{
   render() {
     return (
       <View>
-      <Text style={styles.header}>There we go</Text>
+        <PrayersView name="Sala za Kawaida"></PrayersView>
+        <CategoryDetail name="Akiba" > </CategoryDetail>
+        <CategoryDetail name="Asubuhi" > </CategoryDetail>
       </View>
       );
   }
@@ -13,8 +48,24 @@ export default class ListViewDemo extends Component{
 
 const styles = StyleSheet.create({
   header:{
-    fontSize:20,
-  }
+    fontSize:22,
+    backgroundColor:'steelblue',
+    color:'white',
+    textAlign:'center',
+  },
+  detailheader:{
+   color:'gray',
+   backgroundColor:'lightgray',
+   padding:5,
+   height:30,
+ },
+ detail:{
+  padding:5,
+  height:30,
+  color:'black',
+},
+inputItem:{
+}
 });
 
-AppRegistry.registerComponent('AwesomeProject', () => ListViewDemo);
+AppRegistry.registerComponent('AwesomeProject', () => prayers);
